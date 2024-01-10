9ed164466021e862da88007d74fe2edb29ca4a93
manon-rittling <manon.rittling@laplateforme.io>
2024-01-10 16:01:44 +0100

    interface et boutons ok

--- classes/Menu_principal.py
+++ classes/Menu_principal.py
@@ -3,6 +3,7 @@ import pygame
 from pygame.locals import *
 import sys
 
+
 # classe Menu_principal
 class Menu_principal:
     # constructeur de la classe
@@ -10,7 +11,7 @@ class Menu_principal:
         pygame.init()
         # Créer la fenêtre
         self.__fenetre = pygame.display.set_mode((800, 800))
-        pygame.display.set_caption("Menu Principal")
+        pygame.display.set_caption("Pokemon Arena-Fighter")
         self.__bg = pygame.image.load("images/background/bg_menu_principal.png").convert()
 
         # cree titre du jeu
@@ -26,62 +27,103 @@ class Menu_principal:
         self.__titre_secondaire_rect.y = 165
 
         # Charger la police
-        self.police = pygame.font.Font("police/pokemon Solid.ttf", 25)
+        self.police = pygame.font.Font("police/pokemon Solid.ttf", 30)
 
-        # Faire boutons
-        self.bouton_lancer_jeu = self.police.render("Lancer la partie", True, (0, 0, 0))
-        self.bouton_lancer_jeu_rect = self.bouton_lancer_jeu.get_rect() 
-        self.bouton_lancer_jeu_rect.x = 300
-        self.bouton_lancer_jeu_rect.y = 250
+    
+        # Bouton lancer le jeu
+        self.lancer_jeu = self.police.render("Lancer le jeu", True, (0, 0, 0))
+        self.lancer_jeu_rect = self.lancer_jeu.get_rect()
+        self.lancer_jeu_rect.x = 300
+        self.lancer_jeu_rect.y = 250
 
-        self.bouton_ajouter_pokemon = self.police.render("Ajouter Pokémon", True, (0, 0, 0))
-        self.bouton_ajouter_pokemon_rect = self.bouton_ajouter_pokemon.get_rect()
-        self.bouton_ajouter_pokemon_rect.x = 300
-        self.bouton_ajouter_pokemon_rect.y = 300
+        # Bouton ajouter pokemon
 
-        self.bouton_acces_pokedex = self.police.render("Accès Pokédex", True, (0, 0, 0))
-        self.bouton_acces_pokedex_rect = self.bouton_acces_pokedex.get_rect()
-        self.bouton_acces_pokedex_rect.x = 300
-        self.bouton_acces_pokedex_rect.y = 350
+        self.ajouter_pokemon = self.police.render("Ajouter un pokemon", True, (0, 0, 0))
+        self.ajouter_pokemon_rect = self.ajouter_pokemon.get_rect()
+        self.ajouter_pokemon_rect.x = 300
+        self.ajouter_pokemon_rect.y = 300
 
-        self.bouton_quitter = self.police.render("Quitter", True, (0, 0, 0))
-        self.bouton_quitter_rect = self.bouton_quitter.get_rect()
-        self.bouton_quitter_rect.x = 300
-        self.bouton_quitter_rect.y = 400
+        # bouton pokedex
+        self.pokedex = self.police.render("Pokedex", True, (0, 0, 0))
+        self.pokedex_rect = self.pokedex.get_rect()
+        self.pokedex_rect.x = 300
+        self.pokedex_rect.y = 350
 
-    # Méthode pour afficher le menu principal
-    def afficher_menu_principal(self):
+        self.couleur_origine = (55, 93, 170)
+        self.couleur_changement = (255, 255, 255) 
+
+
+    # méthode afficher
+    def afficher_menu(self):
         self.__fenetre.blit(self.__bg, (0, 0))
         self.__fenetre.blit(self.__titre_principal, self.__titre_principal_rect)
         self.__fenetre.blit(self.__titre_secondaire, self.__titre_secondaire_rect)
-        self.__fenetre.blit(self.bouton_lancer_jeu, self.bouton_lancer_jeu_rect)
-        self.__fenetre.blit(self.bouton_ajouter_pokemon, self.bouton_ajouter_pokemon_rect)
-        self.__fenetre.blit(self.bouton_acces_pokedex, self.bouton_acces_pokedex_rect)
-        self.__fenetre.blit(self.bouton_quitter, self.bouton_quitter_rect)
+        
+        if self.lancer_jeu_rect.collidepoint(pygame.mouse.get_pos()):
+            self.lancer_jeu = self.police.render("Lancer le jeu", True, self.couleur_changement)
+        else:
+            self.lancer_jeu = self.police.render("Lancer le jeu", True, self.couleur_origine)
+        self.__fenetre.blit(self.lancer_jeu, self.lancer_jeu_rect)
 
-        # dessiner les contours des boutons
-        pygame.draw.rect(self.__fenetre, (0, 0, 0), self.bouton_lancer_jeu_rect, 3)
-        pygame.draw.rect(self.__fenetre, (0, 0, 0), self.bouton_ajouter_pokemon_rect, 3)
-        pygame.draw.rect(self.__fenetre, (0, 0, 0), self.bouton_acces_pokedex_rect, 2)
-        pygame.draw.rect(self.__fenetre, (0, 0, 0), self.bouton_quitter_rect, 2)
+        if self.ajouter_pokemon_rect.collidepoint(pygame.mouse.get_pos()):
+            self.ajouter_pokemon = self.police.render("Ajouter un pokemon", True, self.couleur_changement)
+        else:
+            self.ajouter_pokemon = self.police.render("Ajouter un pokemon", True, self.couleur_origine)
+        self.__fenetre.blit(self.ajouter_pokemon, self.ajouter_pokemon_rect)
 
+        if self.pokedex_rect.collidepoint(pygame.mouse.get_pos()):
+            self.pokedex = self.police.render("Pokedex", True, self.couleur_changement)
+        else:
+            self.pokedex = self.police.render("Pokedex", True, self.couleur_origine)
+        self.__fenetre.blit(self.pokedex, self.pokedex_rect)
 
         pygame.display.flip()
 
 
-# Créer une instance de la classe Menu_principal
-menu_principal = Menu_principal()
+# test de la classe
+menu = Menu_principal()
 
-# Afficher le menu principal
-menu_principal.afficher_menu_principal()
-
-# Boucle principale
 running = True
 while running:
+    menu.afficher_menu()
     for event in pygame.event.get():
         if event.type == QUIT:
             running = False
             pygame.quit()
             sys.exit()
+        elif event.type == MOUSEBUTTONDOWN:
+            if event.button == 1:
+                if menu.lancer_jeu_rect.collidepoint(event.pos):
+                    print("lancer le jeu")
+                elif menu.ajouter_pokemon_rect.collidepoint(event.pos):
+                    print("ajouter un pokemon")
+                elif menu.pokedex_rect.collidepoint(event.pos):
+                    print("pokedex")
+        
 
-    pygame.display.update()
+
+
+
+    
+
+            
+
+
+
+
+
+        
+
+
+
+
+
+
+
+
+       
+
+        
+        
+
+   
