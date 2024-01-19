3e7664b7538d5b25949b98724b55c73f8eafd234
manon-rittling <manon.rittling@laplateforme.io>
2024-01-19 12:59:18 +0100

    version1 ok finition a faire

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
 import pygame
 from pygame.locals import *
 import sys
-from Ajouter_pokemon import *
+from classes.Ajouter_pokemon import *
+from classes.Pokedex import *
+from classes.Nouvelle_partie import *
 
 
 def lancer_jeu():
     pygame.init()
 
+    
     menu = Menu_principal()
     ajouter = Ajouter_pokemon()
+    acces_pokedex = None
+    combat = Nouvelle_partie(800,800)
 
     while True:
         for event in pygame.event.get():
@@ -20,6 +25,7 @@ def lancer_jeu():
                     if menu.lancer_jeu_rect.collidepoint(event.pos): 
                         print("lancer le jeu")
                         menu.son_bouton.play()
+                        combat.afficher_fenetre()
                         # Ajoutez le code pour lancer le jeu ici
                     elif menu.ajouter_pokemon_rect.collidepoint(event.pos):
                         print("ajouter un pokemon")
@@ -29,8 +35,11 @@ def lancer_jeu():
                     elif menu.pokedex_rect.collidepoint(event.pos):
                         print("pokedex")
                         menu.son_bouton.play()
+                        acces_pokedex = Pokedex(800,800)
+                        acces_pokedex.menuPokedex()
 
         menu.afficher_menu()
         pygame.display.flip()
+        
 if __name__ == "__main__":
     lancer_jeu()
